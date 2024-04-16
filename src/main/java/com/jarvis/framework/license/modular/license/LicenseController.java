3f09c52ff04dfca4f7f94b152da728d4872428e0
Doug Wang <wangtao@deehero.com>
2024-04-16 11:31:52 +0800

    first commit

---
+++ src/main/java/com/jarvis/framework/license/modular/license/LicenseController.java
@@ -0,0 +1,14 @@
+package com.jarvis.framework.license.modular.license;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Doug Wang
+ * @since 1.8, 2024-04-16 09:41:51
+ */
+@RestController
+@RequestMapping("/generate")
+public class LicenseController {
+
+}
